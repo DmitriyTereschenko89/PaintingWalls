87f3456db41f2968772f29d10559019fa0f1880d
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-10-14 11:38:12 +0300

    update solution

--- Program.cs
+++ Program.cs
@@ -27,7 +27,22 @@ public class Solution
 	public int PaintWalls(int[] cost, int[] time)
 	{
 		int n = cost.Length;
-		int[,] dp = new int[n, n + 1];
-		return DFS(dp, cost, time, 0, n, n);
+		//int[,] dp = new int[n, n + 1];
+		//return DFS(dp, cost, time, 0, n, n);
+		int[,] dp = new int[n + 1, n + 1];
+		for (int i = 1; i <= n; ++i)
+		{
+			dp[n, i] = 1000000009;
+		}
+		for (int i = n - 1; i >= 0; --i)
+		{
+			for (int remain = 1; remain <= n; ++remain)
+			{
+				int painted = cost[i] + dp[i + 1, Math.Max(0, remain - 1 - time[i])];
+				int notPainted = dp[i + 1, remain];
+				dp[i, remain] = Math.Min(painted, notPainted);
+			}
+		}
+		return dp[0, n];
 	}
 }
